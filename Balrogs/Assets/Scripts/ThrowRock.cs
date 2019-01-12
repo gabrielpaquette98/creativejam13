fdb3a829e07197ef4083ac7259ce682d9e6e11bf
Olivia You-Tuon <olivia.you-tuon@polymtl.ca>
2019-01-12 11:47:20 -0500

    OYT: Throwing rock script

---
+++ Balrogs/Assets/Scripts/ThrowRock.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowRock : MonoBehaviour
+{
+    const float ROCK_SPEED = 10f;
+    private float speed;
+    private Rigidbody2D rigidBody;
+    public Vector2 facingDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        speed = ROCK_SPEED;
+        facingDirection = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetDirection();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            MoveRock();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+    private void MoveRock()
+    {
+        rigidBody.velocity = facingDirection * speed;
+    }
+    private void GetDirection()
+    {
+        if (!(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))) {}
+        else {
+            Vector2 direction = Vector2.zero;
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                direction += Vector2.up;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                direction += Vector2.down;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                direction += Vector2.left;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                direction += Vector2.right;
+            }
+            facingDirection = direction;
+        }
+    }
+}
