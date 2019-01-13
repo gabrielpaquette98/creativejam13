250b07becb517f71e78c65bb09c23d554d2424a9
Gabriel Paquette <gabrielpaquette98@gmail.com>
2019-01-12 20:43:12 -0500

    GP: Ready for assembly

--- Balrogs/Assets/Scripts/GameRules.cs
+++ Balrogs/Assets/Scripts/GameRules.cs
@@ -21,7 +21,7 @@ public class GameRules : MonoBehaviour
     void Update()
     {
         //Quand le joueur entre en collision avec un tilemap collider de tag "Exit", on sort du niveau pour en faire un nouveau
-        //A la génération de terrain, le joueur doit apparaitre au spawn point du prefab de tag Spawn pour le premier niveau, Entry pour les autres 
+        //A la génération de terrain, le joueur doit apparaitre au spawn point du prefab de tag Spawn pour le premier niveau, Entry pour les autres levels suivants
         //On doit lier les entrées et sorties (warp zones?)
         
     }
