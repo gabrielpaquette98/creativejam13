33eebfef876caeafc18e22e80689611696c16e56
Olivia You-Tuon <olivia.you-tuon@polymtl.ca>
2019-01-13 14:30:13 -0500

    OYT: Added pause menu

--- Balrogs/Assets/Scripts/InGameCanvas.cs
+++ Balrogs/Assets/Scripts/InGameCanvas.cs
@@ -1,19 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class InGameCanvas : MonoBehaviour
 {
     public GameObject pausePanel;
+    private bool isPaused;
+
     // Start is called before the first frame update
     void Start()
     {
         pausePanel.SetActive(false);
+        isPaused = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                DisplayPausePanel();
+            }
+            else if (isPaused)
+            {
+                ClosePausePanel();
+            }
+        }
+    }
 
+    public void DisplayPausePanel()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0.0f;
+        isPaused = true;
+    }
+
+    public void ClosePausePanel()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1.0f;
+        isPaused = false;
     }
 }
