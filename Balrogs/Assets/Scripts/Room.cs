62c47e477d6392b8351ac8860acde9d225b73261
Gabriel Paquette <gabrielpaquette98@gmail.com>
2019-01-12 20:05:05 -0500

    GP: Ajout des prefab pour les spawns et escaliers

--- Balrogs/Assets/Scripts/Room.cs
+++ Balrogs/Assets/Scripts/Room.cs
@@ -9,7 +9,7 @@ public enum RoomType
 public class Room
 {
     public Vector2Int GridPosition { get; private set; }
-    RoomType CurrentRoomType { get; set; }
+    public RoomType CurrentRoomType { get; private set; }
     bool[] ExitDirections { get; set; }
 
     public bool HasExitUp { get; set; }
@@ -17,31 +17,67 @@ public class Room
     public bool HasExitDown { get; set; }
     public bool HasExitLeft { get; set; }
 
-
+    bool isSpawn;
 
     public Room(Vector2Int position, RoomType roomType) {
         GridPosition = position;
         CurrentRoomType = roomType;
 
         HasExitUp = false;
-        HasExitDown = false; 
+        HasExitDown = false;
         HasExitLeft = false;
         HasExitRight = false;
+        
 }
     public List<Vector2Int> GetNeighboors()
     {
         return GetNeighboors(GridPosition);
     }
+
     public static List<Vector2Int> GetNeighboors(Vector2Int roomPosition)
+    {
+        List<Vector2Int> neighboors = new List<Vector2Int>();
+        if (roomPosition.y > 0)
+        {
+            neighboors.Add(new Vector2Int(roomPosition.x, roomPosition.y - 1));
+        }
+        if (roomPosition.x > 0)
+        {
+            neighboors.Add(new Vector2Int(roomPosition.x - 1, roomPosition.y));
+        }
+        if (roomPosition.x < 7)
+        {
+            neighboors.Add(new Vector2Int(roomPosition.x + 1, roomPosition.y));
+        }
+        if (roomPosition.y < 7)
+        {
+            neighboors.Add(new Vector2Int(roomPosition.x, roomPosition.y + 1));
+        }
+        return neighboors;
+    }
+    public static List<Vector2Int> GetNeighboorsButNoTop(Vector2Int roomPosition)
+    {
+        List<Vector2Int> neighboors = new List<Vector2Int>();
+        if (roomPosition.y > 0)
+        {
+            neighboors.Add(new Vector2Int(roomPosition.x, roomPosition.y - 1));
+        }
+        if (roomPosition.x > 0)
+        {
+            neighboors.Add(new Vector2Int(roomPosition.x - 1, roomPosition.y));
+        }
+        if (roomPosition.x < 7)
+        {
+            neighboors.Add(new Vector2Int(roomPosition.x + 1, roomPosition.y));
+        }
+        return neighboors;
+    }
+    public static List<Vector2Int> GetNeighboorsButNoBottom(Vector2Int roomPosition)
     {
         List<Vector2Int> neighboors = new List<Vector2Int>();
         if (roomPosition.x > 0)
         {
             neighboors.Add(new Vector2Int(roomPosition.x - 1, roomPosition.y));
-        }
-        if (roomPosition.y > 0)
-        {
-            neighboors.Add(new Vector2Int(roomPosition.x, roomPosition.y - 1));
         }
         if (roomPosition.x < 7)
         {
